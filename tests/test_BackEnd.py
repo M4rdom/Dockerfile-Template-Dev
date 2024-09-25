53939981c30b3abc9d744e3572049866be1bed62
Marcos <80154421+M4rdom@users.noreply.github.com>
2024-09-25 17:04:37 +0200

    Added backend test_1

--- tests/test_BackEnd.py
+++ tests/test_BackEnd.py
@@ -1,6 +1,6 @@
 import pytest
 from tests.dockerfile_Generation import assert_Dockerfile_Generation, list_test
 
-@pytest.mark.parametrize("Test_Number", list_test('BackEnd'))
+@pytest.mark.parametrize("Test_Number", list_test('Backend'))
 def test_FrontEnd(Test_Number):
-    assert_Dockerfile_Generation('FrontEnd', Test_Number)
+    assert_Dockerfile_Generation('Backend', Test_Number)
